b192a0abd43f203e202c2115f23a5c37e9f5cefa
brusoth09 <brusoth09@email.com>
2016-05-20 15:07:36 +0530

    struts 2 examples

---
+++ src/main/java/com/atuts/struts/action/WelcomeUserAction.java
@@ -0,0 +1,18 @@
+package com.atuts.struts.action;
+
+
+public class WelcomeUserAction {
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String execute(){
+        return "SUCCESS";
+    }
+}
